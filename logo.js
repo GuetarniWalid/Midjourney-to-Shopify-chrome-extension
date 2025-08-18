766345be456ca531bf359d65cf35f6720187f331
walid GUETARNI <guetarni.walid@gmail.com>
2025-08-18 09:56:50 +0200

    feat(logo): position logo to the right on card to prevent button overlap on Mac

--- logo.js
+++ logo.js
@@ -4,7 +4,7 @@
  */
 function createLogoElement() {
   const div = document.createElement('div');
-  div.className = 'absolute p-2 top-3 left-3 z-10 cursor-pointer chatGPT-logo buttonHoverBackground buttonHoverOpacity rounded-full  active:!ring-white/50 buttonActiveRing buttonActiveOpacity buttonActiveBackground !transition-none active:!transition';
+  div.className = 'absolute p-2 top-3 right-3 z-10 cursor-pointer chatGPT-logo buttonHoverBackground buttonHoverOpacity rounded-full  active:!ring-white/50 buttonActiveRing buttonActiveOpacity buttonActiveBackground !transition-none active:!transition';
   div.innerHTML = `
     <svg class="pointer-events-none" width="18" height="18" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M8.00004 7.49994C8.00004 7.49994 7.50007 3.9999 12.5 2.49994C17.5 0.999987 19.5 3.99994 20 4.49995C20.5 4.99997 23 2.99994 27 6.49994C31.0001 9.99994 28 13.1314 28 13.4999C28.0001 13.8685 31 14.5001 30.5 19.5C30 24.4999 24.5 24.9999 24.5 24.9999C24.5 24.9999 24.5 29.4999 19.5 30.5C14.5 31.5001 13 27.9999 13 27.9999C13 27.9999 8.50007 29.4999 5.50003 25.9999C2.5 22.5 4.50003 18.9999 4.50003 18.9999C4.50003 18.9999 1.00006 17.5 2.50003 11.9999C4 6.49988 8.00004 7.49994 8.00004 7.49994Z" fill="white"/>
